5219181641db2d864cf399505a92f2367efba989
keiushimai <chris8886160@gmail.com>
2023-04-18 17:08:19 +0800

    update deploy bat (docker build version)

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,6 @@ cd
 
 cd mn-test-jenkins
 set JAVA_HOME=D:\path\java\openjdk11-windows\java-11-openjdk-redhat\java-11-openjdk-11.0.17.8-2.windows.redhat.x86_64
-call gradlew runShadow
+call gradlew dockerBuild
+
+call docker run -d -p 8081:8081 mn-test-jenkins
