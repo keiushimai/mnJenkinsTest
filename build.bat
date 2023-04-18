36a3b85a2a33652b818baf7e102b19924a4f2926
keiushimai <chris8886160@gmail.com>
2023-04-18 15:48:37 +0800

    override build bat v4

--- build.bat
+++ build.bat
@@ -5,8 +5,10 @@ cd mn-test-jenkins
 call gradlew clean
 call gradlew shadowJar
 
-cd build
-cd libs
+echo has successfully build shadowJar
 
-set JAVA_HOME=D:\path\java\openjdk11-windows\java-11-openjdk-redhat\java-11-openjdk-11.0.17.8-2.windows.redhat.x86_64
-call java -jar mn-test-jenkins-0.1-all.jar
+REM cd build
+REM cd libs
+
+REM set JAVA_HOME=D:\path\java\openjdk11-windows\java-11-openjdk-redhat\java-11-openjdk-11.0.17.8-2.windows.redhat.x86_64
+REM call java -jar mn-test-jenkins-0.1-all.jar
