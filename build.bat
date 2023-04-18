891518550ed066c051bd5877925f99c5c2807a7e
keiushimai <chris8886160@gmail.com>
2023-04-18 15:36:25 +0800

    override build bat v3

--- build.bat
+++ build.bat
@@ -3,4 +3,10 @@ cd
 cd mn-test-jenkins
 
 call gradlew clean
-call gradlew run
+call gradlew shadowJar
+
+cd build
+cd libs
+
+set JAVA_HOME=D:\path\java\openjdk11-windows\java-11-openjdk-redhat\java-11-openjdk-11.0.17.8-2.windows.redhat.x86_64
+call java -jar mn-test-jenkins-0.1-all.jar
